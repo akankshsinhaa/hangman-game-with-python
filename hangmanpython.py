f945a4481c6a6a830e66d2da7591374a1b57837a
Akanksh <akanksh.work@gmail.com>
2022-10-08 00:02:42 +0530

    adding tkinter functionality for the app

--- hangmanpython.py
+++ hangmanpython.py
@@ -1,28 +1,52 @@
 import random
 f=random.randint(0,4)
+from tkinter import *
+from tkinter import ttk
+Main_window = Tk()
+
+def tryGuess(string):
+    ctr=0
+    n=10
+    if n>2:
+        while ctr<n:
+            x=string
+            if x.isalpha():
+             if x.lower()==secw.lower():
+                return(("you won!"))
+                break
+             elif x!=secw:
+                 for i in x:
+                  if i in secw:
+                     return(i)
+                 ctr+=1
+             else:
+                ctr+=1
+             if ctr==2:
+                r=input("do you want a hint?: ").lower()
+                if r=="y":
+                    return(("%c") % (secw[0]))
+            else:
+                return(("Invalid input"))
+        if ctr==n:
+         return(("you lost!"))
+
 tp=("apple","banana","orange","pears","pomegranate","kiwi")
 secw=tp[f]
-ctr=0
-n=int(input("enter no of tries: "))
-if n>2:
-    while ctr<n:
-        x=input("enter guess: ")
-        if x.isalpha():
-         if x.lower()==secw.lower():
-            print("you won!")
-            break
-         elif x!=secw:
-             for i in x:
-              if i in secw:
-                 print(i, end="")
-             ctr+=1
-         else:
-            ctr+=1
-         if ctr==2:
-            r=input("do you want a hint?: ").lower()
-            if r=="y":
-                print("%c",endl = "\n") % (secw[0])
-        else:
-            print("Invalid input")
-    if ctr==n:
-     print("you lost!")
+def display_text():
+   global entry
+   string= entry.get()
+   
+   label.configure(tryGuess(string))
+
+label=Label(Main_window, text="", font=("Courier 22 bold"))
+label.pack()
+
+entry = Entry(Main_window, width=40)
+entry.focus_set()
+entry.pack()
+ttk.Button(Main_window, text= "Try",width= 20, command= display_text).pack(pady=20)
+
+Main_window.mainloop()
+
+
+
